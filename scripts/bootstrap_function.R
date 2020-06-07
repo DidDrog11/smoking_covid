3e9b641dbf04eae44b67ed3ebe9c228ea3d3a687
DidDrog11 <dsimons1987@gmail.com>
2020-06-07 18:22:29 +0100

    graph done

--- scripts/bootstrap_function.R
+++ scripts/bootstrap_function.R
@@ -6,3 +6,15 @@ boot.mean = function(x,B,binwidth=NULL) {
   interval=quantile(boot.statistics,c(0.025,0.975))
   return(list(boot.statistics=boot.statistics,interval=interval,se=se))
 }
+
+do_bootstrap_current <- function (row_number) {
+  my_sample <- c(rep(1, each = bootstrap$bs_current[row_number]), rep(0, each = bootstrap$bs_inverse[row_number]))
+  bootstrap_output_c[[row_number]] <- boot.mean(my_sample,1000)$interval
+  return(bootstrap_output_c)
+}
+
+do_bootstrap_former <- function (row_number) {
+  my_sample <- c(rep(1, each = bootstrap$bs_former[row_number]), rep(0, each = bootstrap$bs_inverse_former[row_number]))
+  bootstrap_output_f[[row_number]] <- boot.mean(my_sample,1000)$interval
+  return(bootstrap_output_f)
+}
