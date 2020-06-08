904532520369e780bbaab66f2333856f11102dfd
DidDrog11 <dsimons1987@gmail.com>
2020-06-08 14:02:23 +0100

    updated graphs

--- scripts/smoking_plots.R
+++ scripts/smoking_plots.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggsci)
 library(matrixStats)
+library(gapminder)
 
 ## set seed
 set.seed(123)
@@ -11,6 +12,17 @@ extrafont::loadfonts(device = "win")
 
 ## get data
 data <-  read_rds(here::here('data_clean', 'country_prevalence_data.rds'))
+country_colour <- data.frame(gapminder, cc = I(country_colors[match(gapminder$country, names(country_colors))]))
+country_colour <- country_colour %>%
+  select(country, cc) %>%
+  mutate(country = recode(country,
+         "United Kingdom" = "UK",
+         "United States" = "USA",
+         "Korea, Dem. Rep." = "Korea")) %>%
+  rename("country_colour" = cc) %>%
+  distinct() %>%
+  filter(country %in% data$country) %>%
+  deframe()
 
 ## sort data
 sorted_data <- data %>%
@@ -23,7 +35,7 @@ sorted_data <- data %>%
   mutate(mean = ifelse(is.na(weightedMean(prevalence, w = true_sample, idxs = c(smoking, country, study))), mean(prevalence),
                          weightedMean(prevalence, w = true_sample, idxs = c(smoking, country, study))),
          running_count = row_number(),
-         prevalence_country = prevalence_country - 1)
+         prevalence_country = prevalence_country - 1) 
 
 sorted_data$running_count <- as_factor(sorted_data$running_count)  
 
@@ -32,7 +44,9 @@ current_sorted_data <- sorted_data %>%
   arrange(-prevalence, .by_group = T)
 
 a <- ggplot(data = filter(current_sorted_data, study == 1), aes(x = running_count, y = prevalence, colour = country)) +
-  geom_point() +
+  scale_colour_manual(values = country_colour) +
+  geom_point(aes(size = true_sample)) + 
+  scale_size_continuous(trans = "log10") +
   geom_linerange(aes(ymin = lower_ci, ymax =upper_ci)) +
   geom_segment(data = filter(current_sorted_data, study == 1), aes(x = 0, 
                                                                    xend = prevalence_country + 0.5,
@@ -46,19 +60,17 @@ a <- ggplot(data = filter(current_sorted_data, study == 1), aes(x = running_coun
                size = 0.8) +
   scale_y_continuous(name = "Prevalence", limits = c(0, 0.7), breaks = scales::pretty_breaks(n = 9), expand = c(0, 0)) +
   coord_flip() +
-  facet_grid(country ~  ., scales = "free_y", space = "free") +
-  theme(panel.spacing = unit(0, "lines"),
-        strip.text.y = element_text(angle = 0),
+  facet_grid(country ~  ., scales = "free_y", space = "free", switch = 'y') +
+  theme(panel.spacing = unit(0.1, "lines"),
+        strip.text = element_text(angle = 0, size = 12),
         axis.title = element_text(size = 10),
         axis.text.y = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks.y = element_blank(),
         axis.text.x = element_text(size = 10),
         panel.background = element_blank(),
-        strip.background = element_rect(colour = "black", fill = "white", 
-                                          size = 0.5, linetype = "solid"),
-        panel.border = element_rect(color = "black", fill = NA, size = 0.2),
-        axis.line = element_line(colour = "black"),
+        axis.line.x = element_line(colour = "black"),
+        strip.text.y.left = element_text(angle = 0),
         legend.position = "none") +
   labs(title ="Prevalence of current smoking in included studies")
 a
@@ -74,7 +86,9 @@ former_sorted_data <- sorted_data %>%
   arrange(-prevalence, .by_group = T)
 
 a <- ggplot(data = filter(former_sorted_data, study == 1), aes(x = running_count, y = prevalence, colour = country)) +
-  geom_point() +
+  scale_colour_manual(values = country_colour) +
+  geom_point(aes(size = true_sample)) + 
+  scale_size_continuous(trans = "log10") +
   geom_linerange(aes(ymin = lower_ci, ymax =upper_ci)) +
   geom_segment(data = filter(former_sorted_data, study == 1), aes(x = 0, 
                                                                    xend = prevalence_country + 0.5,
@@ -88,19 +102,17 @@ a <- ggplot(data = filter(former_sorted_data, study == 1), aes(x = running_count
                size = 0.8) +
   scale_y_continuous(name = "Prevalence", limits = c(0, 0.7), breaks = scales::pretty_breaks(n = 9), expand = c(0, 0)) +
   coord_flip() +
-  facet_grid(country ~  ., scales = "free_y", space = "free") +
-  theme(panel.spacing = unit(0, "lines"),
-        strip.text.y = element_text(angle = 0),
+  facet_grid(country ~  ., scales = "free_y", space = "free", switch = 'y') +
+  theme(panel.spacing = unit(0.1, "lines"),
+        strip.text = element_text(angle = 0, size = 12),
         axis.title = element_text(size = 10),
         axis.text.y = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks.y = element_blank(),
         axis.text.x = element_text(size = 10),
         panel.background = element_blank(),
-        strip.background = element_rect(colour = "black", fill = "white", 
-                                        size = 0.5, linetype = "solid"),
-        panel.border = element_rect(color = "black", fill = NA, size = 0.2),
-        axis.line = element_line(colour = "black"),
+        axis.line.x = element_line(colour = "black"),
+        strip.text.y.left = element_text(angle = 0),
         legend.position = "none") +
   labs(title ="Prevalence of former smoking in included studies")
 a
